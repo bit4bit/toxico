608b4ad6cea8ae6afcb66b9c00626247fbbc422d
Jovany Leandro G.C <bit4bit@riseup.net>
2021-06-19 20:46:10 -0500

    chore: C version use unsigned int

--- c_src/toxico/tox.spec.exs
+++ c_src/toxico/tox.spec.exs
@@ -3,4 +3,4 @@ module Tox
 interface CNode
 
 
-spec version() :: {:ok :: label, { major :: int, minor :: int, patch :: int}} | :error
+spec version() :: {:ok :: label, { major :: unsigned, minor :: unsigned, patch :: unsigned}} | :error
